fixed calculation of mean in consistency in next window calculations

code/visualization_scripts/create_filtered_multiplex.py
@@ -25,8 +25,8 @@ if len(blacklistedROIs) > 0:
 pooledDataSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/spatial_consistency/pooled_spatial_consistency_data_filtered_multiplex.pkl'
 
 if __name__=='__main__':
-    pooledConsistencies = [[] for i in range(len(filteredJobLabels) + 1)] # the list will contain two sets of consistencies for each filtered parcellation + the reference consistencies
-    pooledROISizes = [[] for i in range(len(filteredJobLabels) + 1)]
+    pooledConsistencies = [[] for i in range(len(filteredJobLabels)*2 + 1)] # the list will contain the original multiplex, filtered multiplex and reference
+    pooledROISizes = [[] for i in range(len(filteredJobLabels)*2 + 1)]
     nRefROIs = []
     nRefSingles = []
     meanNROIs = []
@@ -35,10 +35,13 @@ if __name__=='__main__':
     stdNSingles = []
     meanSizes = []
     stdSizes = []
+    totRefSizes = [[] for i in range(len(filteredJobLabels))]
 
-    for i, filteredJobLabel in enumerate(filteredJobLabels):
+    for i, filteredJobLabel in zip(np.arange(0, len(filteredJobLabels)*2, 2), filteredJobLabels): 
         nROIs = []
         nSingles = []
+        origNROIs = []
+        origNSingles = []
         for subjId in subjectIds:
             for runNumber in runNumbers:
                 referencePath = consistencySaveStem + '/' + subjId + '/' + str(runNumber) + '/' + referenceJobLabel + '/' + str(nLayers) + '_layers' + '/spatial_consistency.pkl'
@@ -119,39 +122,50 @@ if __name__=='__main__':
                         ROISizes = np.array(layer['ROI_sizes'].values())[consistencyIndices[::-1]]
 
                     refNVoxels = np.sum(refROISizes)
+                    totRefSizes[i].append(refNVoxels)
                     nVoxels = 0
                     index = 0
+                    pooledConsistencies[i+1].extend(consistencies)
+                    pooledROISizes[i+1].extend(ROISizes)
+                    origNROIs.append(len(ROISizes))
+                    origNSingles.append(np.sum(np.array(ROISizes)==1))
                     while nVoxels <= refNVoxels:
                         pooledConsistencies[i].append(consistencies[index])
                         pooledROISizes[i].append(ROISizes[index])
                         nVoxels += ROISizes[index]
                         index += 1
-                    pooledConsistencies[-1].extend(refConsistencies) # the last element of the lists contains the consistency and ROI size of the reference parcellation
-                    pooledROISizes[-1].extend(refROISizes)
-                    nROIs.append(len(ROISizes))
-                    nSingles.append(np.sum(np.array(ROISizes) == 1))
-                    if i == len(filteredJobLabels) - 1:
+                    nROIs.append(index-1)
+                    nSingles.append(np.sum(np.array(ROISizes[0:index]) == 1))
+                    if i == len(filteredJobLabels)*2 - 2:
+                        pooledConsistencies[-1].extend(refConsistencies) # the last element of the lists contains the consistency and ROI size of the reference parcellation
+                        pooledROISizes[-1].extend(refROISizes)
                         nRefROIs.append(len(refROISizes))
                         nRefSingles.append(np.sum(np.array(refROISizes) == 1))
-
-                meanNROIs.append(np.mean(nROIs))
-                stdNROIs.append(np.std(nROIs))
-                meanNSingles.append(np.mean(nSingles))
-                stdNSingles.append(np.std(nSingles))
-                sizes = np.array(pooledROISizes[i])
-                meanSizes.append(sizes[sizes>1].mean())
-                stdSizes.append(sizes[sizes>1].std())
-                if i == len(filteredJobLabels) - 1:
-                    meanNROIs.append(np.mean(nRefROIs))
-                    stdNROIs.append(np.std(nRefROIs))
-                    meanNSingles.append(np.mean(nRefSingles))
-                    stdNSingles.append(np.std(nRefSingles))
-                    sizes = np.array(pooledROISizes[-1])
-                    meanSizes.append(sizes[sizes>1].mean())
-                    stdSizes.append(sizes[sizes>1].std())
+        meanNROIs.append(np.mean(nROIs))
+        stdNROIs.append(np.std(nROIs))
+        meanNSingles.append(np.mean(nSingles))
+        stdNSingles.append(np.std(nSingles))
+        sizes = np.array(pooledROISizes[i])
+        meanSizes.append(sizes[sizes>1].mean())
+        stdSizes.append(sizes[sizes>1].std())
+        meanNROIs.append(np.mean(origNROIs))
+        stdNROIs.append(np.std(origNROIs))
+        meanNSingles.append(np.mean(origNSingles))
+        stdNSingles.append(np.std(origNSingles))
+        sizes = np.array(pooledROISizes[i+1])
+        meanSizes.append(sizes[sizes>1].mean())
+        stdSizes.append(sizes[sizes>1].std())        
+        if i == len(filteredJobLabels)*2 - 2:
+           meanNROIs.append(np.mean(nRefROIs))
+           stdNROIs.append(np.std(nRefROIs))
+           meanNSingles.append(np.mean(nRefSingles))
+           stdNSingles.append(np.std(nRefSingles))
+           sizes = np.array(pooledROISizes[-1])
+           meanSizes.append(sizes[sizes>1].mean())
+           stdSizes.append(sizes[sizes>1].std())
 
     f = open(pooledDataSavePath, 'wb')
-    pooledData = {'pooledConsistencies':pooledConsistencies,'ROISizes':ROISizes,'meanNROIs':meanNROIs,'stdNROIs':stdNROIs,'meanNSingles':meanNSingles,'stdNSingles':stdNSingles,'meanSizes':meanSizes,'stdSizes':stdSizes}
+    pooledData = {'pooledConsistencies':pooledConsistencies,'ROISizes':pooledROISizes,'meanNROIs':meanNROIs,'stdNROIs':stdNROIs,'meanNSingles':meanNSingles,'stdNSingles':stdNSingles,'meanSizes':meanSizes,'stdSizes':stdSizes,'nRefVoxels':totRefSizes}
     pickle.dump(pooledData, f)
     f.close()
 

code/visualization_scripts/visualize_spatial_consistency.py
@@ -20,10 +20,10 @@ nLayers = 2
 
 # path parts for reading data
 consistencySaveStem = '/scratch/nbe/alex/private/tarmo/article_runs/maxcorr'
-jobLabels = ['template_brainnetome','craddock','random_balls','ReHo_seeds_weighted_mean_consistency_voxelwise_thresholding_03_regularization-100','ReHo_seeds_min_correlation_voxelwise_thresholding_03'] # This label specifies the job submitted to Triton; there may be several jobs saved under each subject
-clusteringMethods = ['','','','','']
+jobLabels = ['filtered Brainnetome left','filtered Brainnetome right','template','reference'] # This label specifies the job submitted to Triton; there may be several jobs saved under each subject
+clusteringMethods = ['','','','']
 # NOTE: before running the script, check that consistencySaveStem, jobLabel, clusteringMethods, and savePath (specified further below) match your data
-blacklistedROIs = np.arange(211,247)
+blacklistedROIs = []#np.arange(211,247)
 blacklistWholeROIs = True # if True, all ROIs with blacklisted voxels are removed; if False, blacklisted voxels are removed but rest of the ROI kept, which affects size distribution
 windowLength = 80
 windowOverlap = 0
@@ -31,10 +31,10 @@ if len(blacklistedROIs) > 0:
     iniDataFolder = '/scratch/nbe/alex/private/janne/preprocessed_ini_data/'
 
 # path parths for saving
-pooledDataSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/spatial_consistency/pooled_spatial_consistency_data_for_fig_without_subcortical.pkl'
-consFigureSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/consistency_distributions_without_subcortical.pdf'
-sizeFigureSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/size_distributions_without_subcortical.pdf'
-consSizeFigureSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/size_vs_consistency_without_subcortical.pdf'
+pooledDataSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/spatial_consistency/pooled_spatial_consistency_data_filtered_multiplex.pkl'
+consFigureSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/consistency_distributions_filtered_multiplex.pdf'
+sizeFigureSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/size_distributions_filtered_multiplex.pdf'
+consSizeFigureSavePath = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/size_vs_consistency_filtered_multiplex.pdf'
 
 # distribution and visualization parameters
 nConsBins = 50
@@ -44,6 +44,7 @@ alphas = [0.9,0.5,0.9,0.9,0.9]
 excludeSizes = False
 excludeSingleVoxels = True
 stdAlpha = 0.05
+filteredMultiplex = True
 
 # TODO: add an option for removing the last ROI before calculating distributions (lists without the last ROI are defined so that sum(ROISizes) < nRefVoxels)
 
@@ -53,6 +54,9 @@ if os.path.isfile(pooledDataSavePath):
         f.close()
         pooledConsistencies = pooledData['pooledConsistencies']
         ROISizes = pooledData['ROISizes']
+        if filteredMultiplex:
+            pooledConsistencies.insert(1, pooledConsistencies[0]) 
+            ROISizes.insert(1, ROISizes[0])
         meanNROIs = pooledData['meanNROIs']
         stdNROIs = pooledData['stdNROIs']
         meanNSingles = pooledData['meanNSingles']
@@ -193,6 +197,19 @@ if not excludeSizes:
     consSizeAx.set_xscale('log')
 
 for pooledConsistency, sizes, jobLabel, clusteringMethod, color, alpha in zip(pooledConsistencies, ROISizes, jobLabels, clusteringMethods, colors, alphas):
+    leftCounter = 0
+    if 'left' in jobLabel:
+        # the left distribution will contain N ROIs so that their total size is smaller than the number of voxels in the reference parcellation; the right
+        # distribution contains N+1 ROIs
+        nRefVoxels = pooledData['nRefVoxels'][leftCounter]
+        dind = []
+        tempSizes = np.copy(sizes)
+        for ref in nRefVoxels:
+            dind.append(np.where(np.cumsum(tempSizes) > ref)[0][0])
+            tempSizes[:dind[-1]+1] = 0
+        pooledConsistency = np.delete(pooledConsistency, dind)
+        sizes = np.delete(sizes, dind)
+        leftCounter += 1 
     if excludeSingleVoxels:
         sizes = np.array(sizes)
         pooledConsistency = np.array(pooledConsistency)

code/visualization_scripts/visualize_spatial_consistency_in_next_window.py
@@ -6,7 +6,7 @@ import pickle
 import os.path
 import matplotlib.pylab as plt
 import numpy as np
-from scipy.stats import binned_statistic_2d
+from scipy.stats import binned_statistic, binned_statistic_2d
 
 import clustering_by_consistency as cbc
 
@@ -104,13 +104,13 @@ for jobLabel, clusteringMethod, color, alpha in zip(jobLabels, clusteringMethods
     bins = np.linspace(min(min(presentWindowConsistencies), min(nextWindowConsistencies)), max(max(nextWindowConsistencies),max(presentWindowConsistencies)), nBins + 1)
     binCenters = bins[:-1] + (bins[1::] - bins[:-1])/2
     ret = binned_statistic_2d(presentWindowConsistencies, nextWindowConsistencies, presentWindowConsistencies, statistic='count', bins=bins)
-    maxLocation = np.array([binCenters[np.where(stat == np.max(stat))][0] for stat in ret.statistic])
+    averageStat = binned_statistic(presentWindowConsistencies, nextWindowConsistencies, statistic='mean', bins=bins).statistic
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ret.statistic[np.where(ret.statistic == np.amin(ret.statistic))] = 'nan'
-    binnedInfo.append((ret.x_edge, ret.y_edge, binCenters, ret.statistic, maxLocation))
+    binnedInfo.append((ret.x_edge, ret.y_edge, binCenters, ret.statistic, averageStat))
     plt.pcolor(ret.x_edge, ret.y_edge, np.transpose(ret.statistic), cmap=cmap)
-    plt.plot(binCenters, maxLocation, color='c')
+    plt.plot(binCenters, averageStat, color='c')
     ax.set_xlabel('Consistency in present window')
     ax.set_ylabel('Consistency in the window present + %x' %timelag)
     ax.set_title(title)
@@ -124,20 +124,23 @@ for jobLabel, clusteringMethod, color, alpha in zip(jobLabels, clusteringMethods
     fig = plt.figure()
     ax = fig.add_subplot(111)
     dists = np.zeros(ret.statistic.shape)
+    averageStat = np.zeros(ret.statistic.shape[0])
     for i in range(nBins):
         y = list(nextWindowConsistencies[np.where((bins[i] <= np.array(presentWindowConsistencies)) & (np.array(presentWindowConsistencies) < bins[i + 1]))])
         if i == nBins - 1:
             y2 = np.array(nextWindowConsistencies)[np.where(np.array(presentWindowConsistencies) == bins[i + 1])]
             y.extend(y2)
         d, _ = np.histogram(y, bins=bins, density=True)
+        m = np.nanmean(y)
         if np.all(np.isnan(d)):
             d = np.zeros(len(d))
+            m = 0
         dists[:, i] = d
-    maxLocation = np.array([binCenters[np.where(dist == np.max(dist))][0] for dist in dists.T])
+        averageStat[i] = m
     dists[np.where(dists == np.amin(dists))] = 'nan'
-    distInfo.append((ret.x_edge, ret.y_edge, binCenters, dists, maxLocation))
+    distInfo.append((ret.x_edge, ret.y_edge, binCenters, dists, averageStat))
     plt.pcolor(ret.x_edge, ret.y_edge, dists, cmap=cmap)
-    plt.plot(binCenters, maxLocation, color='c')
+    plt.plot(binCenters, averageStat, color='c')
     ax.set_xlabel('Consistency in present window')
     ax.set_ylabel('Consistency in the window present + %x' %timelag)
     ax.set_title(title)
@@ -179,22 +182,22 @@ for presentWindowConsistencies, nextWindowConsistencies, ret, dist, clusteringMe
     binCenters = dist[2][binCenterMask]
 
     zoomedStatistic = ret[3][xmask].T[ymask]
-    zoomedStatisticMaxLocation = ret[4][binCenterMask]
+    zoomedStatisticAverageStat = ret[4][binCenterMask]
     zoomedDist = dist[3][ymask].T[xmask].T # in dist[3], rows correspond to next window, columns to present window
-    zoomedDistMaxLocation = dist[4][binCenterMask]
+    zoomedDistAverageStat = dist[4][binCenterMask]
     if np.any(binCenters > max(x)) or np.any(binCenters < min(x)):
-        zoomedStatisticMaxLocation = zoomedStatisticMaxLocation[np.where((binCenters <= max(x)) & (binCenters >= min(x)))]
-        zoomedDistMaxLocation = zoomedDistMaxLocation[np.where((binCenters <= max(x)) & (binCenters >= min(x)))]
+        zoomedStatisticAverageStat = zoomedStatisticAverageStat[np.where((binCenters <= max(x)) & (binCenters >= min(x)))]
+        zoomedDistAverageStat = zoomedDistAverageStat[np.where((binCenters <= max(x)) & (binCenters >= min(x)))]
         binCenters = binCenters[np.where((binCenters <= max(x)) & (binCenters >= min(x)))]
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.pcolor(x, y, zoomedStatistic, cmap=cmap)
-    plt.plot(binCenters, zoomedStatisticMaxLocation, color='c')
+    plt.plot(binCenters, zoomedStatisticAverageStat, color='c')
     ax.set_xlabel('Consistency in present window')
     ax.set_ylabel('Consistency in the window present + %x' %timelag)
     ax.set_title(title)
-    plt.colorbar(label='PDF(y)')
+    plt.colorbar(label='Count')
     plt.plot([min(x), max(x)], [min(y), max(y)], color='r')
     plt.tight_layout()
     plt.savefig(zoomBinnedSavePath, format='pdf', bbox_inches='tight')
@@ -203,7 +206,7 @@ for presentWindowConsistencies, nextWindowConsistencies, ret, dist, clusteringMe
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.pcolor(x, y, zoomedDist, cmap=cmap)
-    plt.plot(binCenters, zoomedDistMaxLocation, color='c')
+    plt.plot(binCenters, zoomedDistAverageStat, color='c')
     ax.set_xlabel('Consistency in present window')
     ax.set_ylabel('Consistency in the window present + %x' %timelag)
     ax.set_title(title)