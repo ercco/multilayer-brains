0dadd39a11b4c5c15e2f9d59205e5a6768d80a95
Onerva Korhonen <onerva.korhonen@gmail.com>
2024-01-18 12:57:20 +0200

    added a script for creating the schematic figure

---
+++ code/visualization_scripts/schematic_figure.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 17:55:34 2023
+
+@author: onerva
+
+A script for creating the schematic figure (Fig. 1)
+"""
+import nibabel as nib
+import numpy as np
+from surfplot import Plot
+from surfplot.utils import threshold
+from neuromaps.datasets import fetch_fslr, fetch_fsaverage
+from neuromaps.transforms import mni152_to_fslr, mni152_to_fsaverage
+
+#from brainspace.datasets import load_parcellation
+
+load_data = True
+template_path = '/home/onerva/projects/ROIplay/templates/brainnetome/BNA-MPM_thr25_4mm.nii'
+data_path = '/home/onerva/projects/multilayer-meta/epi_preprocessed_standard.nii'
+data_save_path = '/home/onerva/projects/multilayer-meta/article_figs/schematig_fig/epi_preprocessed_standard'
+
+# plotting the Brainnetome ROI boundaries on brain surface
+
+template_img = nib.load(template_path)
+template_data = template_img.get_fdata()
+
+template_gii_lh, template_gii_rh = mni152_to_fslr(template_path, method='nearest')
+template_lh = threshold(template_gii_lh.agg_data(), 3)
+template_rh = threshold(template_gii_rh.agg_data(), 3)
+
+surfaces = fetch_fslr()
+lh, rh = surfaces['inflated']
+sulc_lh, sulc_rh = surfaces['sulc']
+
+p = Plot(lh, rh)
+p.add_layer({'left': sulc_lh, 'right': sulc_rh}, cmap='binary_r', cbar=False)
+p.add_layer({'left': template_lh, 'right': template_rh}, cmap='gray', as_outline=True, cbar=False)
+
+# plotting data
+
+if load_data:
+    data_img = nib.load(data_path)
+    data = data_img.get_fdata()
+
+    data_lh, data_rh = mni152_to_fslr(data)
+    np.save(data_save_path + '_lh', data_lh)
+    np.save(data_save_path + '_rh', data_rh)
+    
+else:
+    data_lh = np.load(data_save_path + '_lh')
+    data_rh = np.load(data_save_path + '_rh')
+    
+data_lh = threshold(data_lh.agg_data(), 3)
+data_rh = threshold(data_rh.agg_data(), 3)
+
+p.add_layer({'left': data_lh, 'right': data_rh})
+
+fig = p.build()
+
+
