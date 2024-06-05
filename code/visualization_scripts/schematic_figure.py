4de5e81022926c7e94c96e195ad195daaa3c578f
Onerva Korhonen <onerva.korhonen@aalto.fi>
2024-06-05 17:02:49 +0300

    added a script for calculating intrinsic scale distributions; results look weird so needs debugging

--- code/visualization_scripts/schematic_figure.py
+++ code/visualization_scripts/schematic_figure.py
@@ -19,10 +19,10 @@ from neuromaps.transforms import mni152_to_fslr, mni152_to_fsaverage
 #from brainspace.datasets import load_parcellation
 
 load_data = True
-template_path = '/m/cs/scratch/networks/aokorhon/ROIplay/templates/brainnetome/BNA-MPM_thr25_4mm.nii'
-data_path = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/schematic_fig/test_slice.nii'
-data_save_path = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/schematic_fig/test_slice'
-figure_save_path = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/schematic_fig/schematic_fig.pdf'
+template_path = '/home/onervak/projects/ROIplay/templates/brainnetome/BNA-MPM_thr25_4mm.nii'
+#data_path = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/schematic_fig/test_slice.nii'
+#data_save_path = '/m/cs/scratch/networks/aokorhon/multilayer/outcome/article_figs/schematic_fig/test_slice'
+figure_save_path = '/home/oenrvak/multilayer_brains/article_figs/schematic_fig.pdf'
 
 # plotting the Brainnetome ROI boundaries on brain surface
 
@@ -43,23 +43,23 @@ p.add_layer({'left': template_lh, 'right': template_rh}, cmap='gray', as_outline
 
 # plotting data
 
-if load_data:
-    data_img = nib.load(data_path)
-    data = data_img.get_fdata()
+#if load_data:
+#    data_img = nib.load(data_path)
+#    data = data_img.get_fdata()
 
-    data_lh, data_rh = mni152_to_fslr(data_path)
-    np.save(data_save_path + '_lh', data_lh)
-    np.save(data_save_path + '_rh', data_rh)
+#    data_lh, data_rh = mni152_to_fslr(data_path)
+#    np.save(data_save_path + '_lh', data_lh)
+#    np.save(data_save_path + '_rh', data_rh)
     
-else:
-    data_lh = np.load(data_save_path + '_lh')
-    data_rh = np.load(data_save_path + '_rh')
+#else:
+#    data_lh = np.load(data_save_path + '_lh')
+#    data_rh = np.load(data_save_path + '_rh')
     
-data_lh = threshold(data_lh.agg_data(), 3)
-data_rh = threshold(data_rh.agg_data(), 3)
+#data_lh = threshold(data_lh.agg_data(), 3)
+#data_rh = threshold(data_rh.agg_data(), 3)
 
-p.add_layer({'left': data_lh, 'right': data_rh})
+#p.add_layer({'left': data_lh, 'right': data_rh})
 
 fig = p.build()
-plt.savefig(figure_save_path, format='pdf',bbox_inches='tight')
+#plt.savefig(figure_save_path, format='pdf',bbox_inches='tight')
 
